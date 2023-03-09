08c50fd6a3baccd8297a2346bd481527e4e81e60
remove <b508099021@tmu.edu.tw>
2023-03-09 14:21:06 +0800

    [Add] adjusted KML files and the second KML Layer-CCTV.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    class="loader loader-default"
-    :class="{ 'is-active': isLoading }"
-    data-text="資料載入中"
-  ></div>
+  <div class="loader loader-default" :class="{ 'is-active': isLoading }" data-text="資料載入中"></div>
   <div id="mapContainer" ref="mapContainer"></div>
 </template>
 
@@ -21,12 +17,16 @@ export default {
     return {
       // maxDN: 0,
       mapInstance: {},
+      // 用來測試套件的假資料
       geoJsonLayerTest: {
         features: [
           {
             type: 'Feature',
             geometry: {
               type: 'MultiPolygon',
+              // 原本的記錄方式會跳錯，所以下面用一支 disbracket() 來替換結構
+              // 注意，原生 leaflet 跟 leaflet-glify (少一層) 能讀的結構似乎不同
+              // 可能也跟 Polygon, MultiPolygon 的記錄方式有關，之後使用要先做測試用資料，確定 OK 再讀取 API
               // coordinates: [
               //   [
               //     [
@@ -66,7 +66,6 @@ export default {
         type: 'FeatureCollection',
         name: 'Mid2_Country_Site',
       },
-      // 先經後緯
       geoJsonLayerTest2: {
         type: 'FeatureCollection',
         features: [
@@ -74,6 +73,7 @@ export default {
             type: 'Feature',
             geometry: {
               type: 'MultiPolygon',
+              // 先經後緯
               coordinates: [
                 [
                   [120.9121, 24.7],
@@ -94,6 +94,7 @@ export default {
         ],
         name: 'Mid2_Country_Site',
       },
+      // the sample data on official page of leaflet-glify
       // geoJsonLayerCzech: {
       //   type: 'FeatureCollection',
       //   features: [
@@ -119,14 +120,14 @@ export default {
       //   ],
       // },
       isLoading: false,
-      kmlLayer: {},
+      kmlLayerIntersection: {},
+      kmlLayerCctv: {},
     };
   },
   methods: {
     getGeoJsonLayer() {
       const vm = this;
-      const url =
-        'https://raw.githubusercontent.com/wiki70170/XDevelop/master/Mid2_Country_Site.geojson';
+      const url = 'https://raw.githubusercontent.com/wiki70170/XDevelop/master/Mid2_Country_Site.geojson';
       // const url = 'src/assets/Mid2_Country_Site.geojson';
       return new Promise(function (resolve, reject) {
         vm.isLoading = true;
@@ -134,7 +135,7 @@ export default {
           .get(url)
           .then(function (response) {
             // vm.displayGeoJson(response.data);
-            vm.displayGeoJsonTest(response.data);
+            vm.displayGeoJsonLeafletGL(response.data);
             resolve(response);
           })
           .catch(function (err) {
@@ -165,29 +166,102 @@ export default {
         // })
         .addTo(vm.mapInstance);
     },
-    displayGeoJsonTest(geoJsonResponse) {
+    displayGeoJsonLeafletGL(geoJsonResponse) {
       const vm = this;
       // console.log('displayTest Executed!');
-      console.log('beforeFormatted', geoJsonResponse);
+      // console.log('beforeFormatted', geoJsonResponse);
       geoJsonResponse.features.forEach((item, index) => {
         vm.disbracket(item);
+        item.properties.color = vm.convertDnToColorLeafletGL(item.properties.DN);
       });
-      console.log('formatted', geoJsonResponse);
+      // console.log('geoJsonResponse', geoJsonResponse);
       // console.log(L.glify.shapes);
       L.glify.shapes({
         map: vm.mapInstance,
         data: geoJsonResponse,
         // data: vm.geoJsonLayerTest,
+        // color: '#00FF00',
+        // vertexShaderSource() {
+        //   console.log(arguments);
+        // },
+        // fragmentShaderSource() {
+        //   console.log(arguments);
+        // },
         color() {
-          return {
-            r: 255,
-            g: 0,
-            b: 0,
-          };
+          // console.log(arguments[1].properties.color);
+          return arguments[1].properties.color;
         },
+        // click(event) {
+        //   console.log('ev', event);
+        //   console.log('arg', arguments);
+        // },
+        // color() {
+        //   return {
+        //     r: 255,
+        //     g: 0,
+        //     b: 0,
+        //   };
+        // },
         // 不透明度，預設為 0.5，全不透是 1
-        opacity: 1,
+        opacity: 0.5,
+        // preserveDrawingBuffer: 0,
+        // borders: true,
       });
+    },
+    convertDnToColorLeafletGL(DN) {
+      let color;
+      if (DN <= 2) {
+        // 152,251,152
+        color = { r: 0.5, g: 1, b: 0.5 };
+      } else if (DN <= 4) {
+        color = { r: 0, g: 1, b: 0 };
+      } else if (DN <= 6) {
+        color = { r: 0, g: 0, b: 1 };
+      } else {
+        color = { r: 1, g: 0, b: 0 };
+      }
+      // switch (DN) {
+      //   case 1:
+      //     // light gray
+      //     color = { r: 220, g: 220, b: 220 };
+      //     break;
+      //   case 2:
+      //     // azure
+      //     color = { r: 240, g: 255, b: 255 };
+      //     break;
+      //   case 3:
+      //     // pale green
+      //     color = { r: 152, g: 251, b: 152 };
+      //     break;
+      //   case 4:
+      //     // khaki
+      //     color = { r: 240, g: 230, b: 140 };
+      //     break;
+      //   case 5:
+      //     // crimson
+      //     color = { r: 220, g: 20, b: 60 };
+      //     break;
+      //   case 6:
+      //     // maroon
+      //     color = { r: 128, g: 0, b: 0 };
+      //     break;
+      //   case 7:
+      //     // violet
+      //     color = { r: 238, g: 130, b: 238 };
+      //     break;
+      //   case 8:
+      //     // purple
+      //     color = { r: 102, g: 51, b: 0 };
+      //     break;
+      //   case 9:
+      //     // navy
+      //     color = { r: 0, g: 0, b: 128 };
+      //     break;
+      //   default:
+      //     color = null;
+      //     break;
+      // }
+      return color;
     },
     convertDNtoColor(item) {
       // 最大為 9
@@ -199,13 +273,13 @@ export default {
           item.properties.color = 'gray';
           break;
         case 2:
-          item.properties.color = 'Green';
+          item.properties.color = 'green';
           break;
         case 3:
-          item.properties.color = 'Yellow';
+          item.properties.color = 'yellow';
           break;
         case 4:
-          item.properties.color = 'Orange';
+          item.properties.color = 'orange';
           break;
         case 5:
           item.properties.color = 'red';
@@ -214,13 +288,13 @@ export default {
           item.properties.color = 'maroon';
           break;
         case 7:
-          item.properties.color = 'purple';
-          break;
-        case 8:
           item.properties.color = 'violet';
           break;
+        case 8:
+          item.properties.color = 'purple';
+          break;
         case 9:
-          item.properties.color = 'Navy';
+          item.properties.color = 'navy';
           break;
         default:
           item.properties.color = null;
@@ -232,7 +306,7 @@ export default {
     disbracket(item) {
       item.geometry.coordinates = item.geometry.coordinates[0];
     },
-    displayKml() {
+    displayIntersectionKml() {
       const vm = this;
       vm.axios.get('src/assets/T61_intersection.kml').then(function (response) {
         let data = response.data;
@@ -241,9 +315,24 @@ export default {
         const parser = new DOMParser();
         // 解析 Kml
         const kmlData = parser.parseFromString(data, 'text/xml');
-        vm.kmlLayer = new L.KML(kmlData);
+        vm.kmlLayerIntersection = new L.KML(kmlData);
 
-        vm.mapInstance.addLayer(vm.kmlLayer);
+        vm.mapInstance.addLayer(vm.kmlLayerIntersection);
+      });
+    },
+    // 懶得改
+    displayCctvKml() {
+      const vm = this;
+      vm.axios.get('src/assets/CCTV_T61.kml').then(function (response) {
+        let data = response.data;
+        // console.log(arguments);
+        // console.log(data);
+        const parser = new DOMParser();
+        // 解析 Kml
+        const kmlData = parser.parseFromString(data, 'text/xml');
+        vm.kmlLayerCctv = new L.KML(kmlData);
+
+        vm.mapInstance.addLayer(vm.kmlLayerCctv);
       });
     },
   },
@@ -257,12 +346,16 @@ export default {
     });
     vm.mapInstance = mapInstance;
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(mapInstance);
+    // console.log(L.glify.shader.fragment.polygon);
+    // console.log(L.glify.shader.vertex);
+
     vm.getGeoJsonLayer();
     // vm.displayGeoJsonTest();
-    vm.displayKml();
+    // icon 目前直接在 kml 中改，由 flaticon 下載後放到公司測試機的路徑下
+    vm.displayIntersectionKml();
+    vm.displayCctvKml();
   },
   created() {},
 };
